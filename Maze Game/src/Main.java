eda240851082668a131a14b0a16e490ab4bdfcd0
mrssrob <105818560+mrssrob@users.noreply.github.com>
2026-08-18 21:40:06 -0400

    Update Main.java
    Final Main class copy and pasted from my IDE

--- Maze Game/src/Main.java
+++ Maze Game/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /*
  *  Sarah Robinson
  *  August 9, 2026
@@ -7,8 +8,42 @@
  */
 public class Main {
 
-    public static void main(String[] args) {
+   public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        Maze m = new Maze();
+        m.display();
+        System.out.println("Use wasd to move. Move the * to the F position");
+        boolean win = false;
+        
+        //continues to loop until checkwin returns false
+        while(win == false)
+        {
+            System.out.println("Enter move:");
+          String dir = scan.nextLine();
     
+    
+            if (dir.equals("w"))
+            {
+                m.moveUp();
+            }
+            else if(dir.equals("a"))
+            {
+                m.moveLeft();
+            }
+            else if(dir.equals("d"))
+            {
+                m.moveRight();
+            }
+            else if(dir.equals("s"))
+            {
+                m.moveDown();
+            }
+            m.display();
+            win = m.checkWin();  
+            }
+        
+        
     }
 
 }
